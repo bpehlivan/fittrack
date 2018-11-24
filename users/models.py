675d0c438ff0cf2be8bfb27a93d2b240b2346ce5
bpehlivan <bpehlivan235@gmail.com>
2018-11-24 23:34:23 +0300

    updated User Model,

--- users/models.py
+++ users/models.py
@@ -6,11 +6,10 @@ from users.enums import Measurement
 
 
 class FitUser(User):
-    township = models.CharField(max_length=50, default='')
-    city = models.CharField(max_length=50, default='')
     register_date = models.DateTimeField(auto_now_add=True)
     last_login_date = models.DateTimeField(auto_now=True)
     height = EnumField(Measurement, default=Measurement.METRIC)
+    is_active = models.BooleanField(_('active'), default=False)
 
     def __str(self):
         return self.username
