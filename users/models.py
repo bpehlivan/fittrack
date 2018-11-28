bf4d84f4e728b668ac09dc6eb346e505a6ab756b
bpehlivan <bpehlivan235@gmail.com>
2018-11-29 00:39:33 +0300

    added UserInfo model

--- users/models.py
+++ users/models.py
@@ -8,9 +8,13 @@ from users.enums import Measurement
 class FitUser(User):
     register_date = models.DateTimeField(auto_now_add=True)
     last_login_date = models.DateTimeField(auto_now=True)
-    height = EnumField(Measurement, default=Measurement.METRIC)
     is_active = models.BooleanField(_('active'), default=False)
 
-    def __str(self):
+    def __str__(self):
         return self.username
 
+
+class UserInfo(models.Model):
+    user = models.ForeignKey(FitUser, on_delete=models.CASCADE)
+    unit_type = EnumField(Measurement, default=Measurement.METRIC)
+    height = models.FloatField()
