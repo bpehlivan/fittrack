2b93c9cc0afaa09f0286edb9912369e1732eb00b
bpehlivan <bpehlivan235@gmail.com>
2018-12-18 00:25:38 +0300

    added base user model

--- users/models.py
+++ users/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 from enumfields import EnumField
 
 from users.enums import Measurement
 
 
-class FitUser(User):
+class FitUser(AbstractUser):
     register_date = models.DateTimeField(auto_now_add=True)
-    last_login_date = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(_('active'), default=False)
+    last_modified_date = models.DateTimeField(auto_now=True)
+    email = models.EmailField(_('email address'), max_length=200,
+                              help_text='required', null=False)
 
     def __str__(self):
         return self.username
