5bc2c43760ba15dc31af0bcc9e8d1e9973972ace
bpehlivan <bpehlivan235@gmail.com>
2018-12-18 00:29:08 +0300

    added user serializers

---
+++ users/resources/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from users.models import FitUser, UserInfo
+
+
+class FitUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FitUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
