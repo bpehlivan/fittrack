df43b3548beab8567ac9d0b16eb6423128bdc9c2
bpehlivan <bpehlivan235@gmail.com>
2018-12-18 00:30:05 +0300

    added token generator for registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+TODO: make registration view
+
+https://stackoverflow.com/questions/50298114/django-2-how-to-register-a-user-using-email-confirmation-and-cbvs
+"""
