05448f7ae532353c2ce1bd86d63968deb1e548da
bpehlivan <bpehlivan235@gmail.com>
2019-01-22 01:00:51 +0300

    enabled caching in project

--- fittrack_project/settings_temp.py
+++ fittrack_project/settings_temp.py
@@ -11,6 +11,20 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://caching:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "fittrack"
+    }
+}
+
+# Cache time to live
+CACHE_TTL = 60 * 15
+
 SECRET_KEY = 'kpm*6ijt=j*i9jh!f87^)n_wgz^)v!e_#pa1ff%_^0^j4f4oy3'
 
 
--- records/views.py
+++ records/views.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from records.models import Record
 from records.serializers import RecordSerializer
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 class RecordViewSet(ModelViewSet):
@@ -15,3 +21,8 @@ class RecordViewSet(ModelViewSet):
     def perform_create(self, serializer):
         validated_data = serializer.validated_data
         Record.objects.create(user=self.request.user, **validated_data)
+
+    @method_decorator(cache_page(CACHE_TTL))
+    def list(self, request, *args, **kwargs):
+        response = super().list(self, request, *args, **kwargs)
+        return response
